5ec13180bb88cfc34fdf7cfc9f580f7fff800e95
Joao <jfjoao12@gmail.com>
2025-05-15 10:57:08 -0500

    Adding FileRow and CustomCardLayout to git

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,5 @@
-import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -11,31 +9,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.onClick
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.ModalDrawer
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.FolderOpen
-import androidx.compose.material.icons.filled.ModeStandby
-import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.*
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.PointerEvent
-import androidx.compose.ui.input.pointer.PointerEventType
-import androidx.compose.ui.input.pointer.onPointerEvent
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.DelicateCoroutinesApi
@@ -43,23 +26,22 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import layout.CustomCardLayout
-import operations.Device
-import operations.FileInfo
 import operations.IpswFileStatus
 import operations.Operations
 import java.io.File
 import java.util.prefs.Preferences
 import javax.swing.JFileChooser
 import javax.swing.UIManager
-import androidx.compose.foundation.combinedClickable
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Outline
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.filled.Download
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.window.WindowPosition
+import androidx.compose.ui.window.rememberWindowState
+import operations.FileRow
 
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalSharedTransitionApi::class)
 @Composable
 @Preview
 fun AppUI(
@@ -67,7 +49,8 @@ fun AppUI(
     files: List<IpswFileStatus>,
     isLoading: Boolean,
     onSelectFolder: () -> Unit,
-    onCheckAndUpdate: () -> Unit
+    onCheckAndUpdate: () -> Unit,
+
 ) {
     MaterialTheme(
         colorScheme = lightColorScheme()
@@ -108,6 +91,38 @@ fun AppUI(
                     CustomCardLayout(
                         "Files Found",
                     ) {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth(),
+
+                        ){
+
+
+                            Row(
+                                horizontalArrangement = Arrangement.End,
+                            ) {
+                                IconButton(
+                                    onClick = onCheckAndUpdate,
+                                    enabled = !isLoading,
+                                    content = {
+                                        Icon(
+                                            imageVector = Icons.Default.Refresh,
+                                            contentDescription = "Check Folder")
+                                    },
+                                )
+                                IconButton(
+                                    onClick = onCheckAndUpdate,
+                                    enabled = !isLoading,
+                                    content = {
+                                        Icon(
+                                            imageVector = Icons.Default.Download,
+                                            contentDescription = "Check Folder")
+                                    },
+                                )
+
+                            }
+                        }
+
                         LazyColumn(
                             modifier = Modifier
                                 .fillMaxWidth(),
@@ -115,13 +130,10 @@ fun AppUI(
                         ) {
                             if (files.isNotEmpty()) {
                                 items(files) { status ->
-                                    FileRow(status)
-                                    HorizontalDivider(
-                                        modifier = Modifier
-                                            .fillMaxWidth(0.3f)
-                                            .align(Alignment.CenterVertically),
+                                    FileRow(
+                                        status,
+                                        isLoading,
                                     )
-
                                 }
 
                             } else {
@@ -153,143 +165,8 @@ fun AppUI(
     }
 }
 
-@OptIn(ExperimentalComposeUiApi::class, ExperimentalFoundationApi::class)
-@Composable
-fun FileRow(device: IpswFileStatus) {
-    val fileName = device.file.name
-    val operations = Operations()
-
-    var deviceInfo: Device? by remember { mutableStateOf<Device?>(null) }
-    var active by remember { mutableStateOf(false) }
-
-    LaunchedEffect(device.file) {
-        deviceInfo = operations.buildDevice(device.file.name)
-    }
 
 
-
-
-        Box(
-            modifier = Modifier
-                .padding(vertical = 2.dp,  horizontal = 12.dp)
-
-        ){
-
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 8.dp, horizontal = 12.dp)
-                    .combinedClickable(
-                        onClick = {
-                            active = !active
-                        },
-                    )
-                    .drawBehind {
-                        // convert 1.dp into pixels
-                        val strokeWidth = 1.dp.toPx()
-                        // left edge
-                        drawLine(
-                            color = Color.Black,
-                            start = Offset(0f, 0f),
-                            end   = Offset(0f, size.height),
-                            strokeWidth = strokeWidth
-                        )
-                    },
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Column(
-                    modifier = Modifier
-                        .padding(8.dp, 4.dp)
-
-                ) {
-
-                    Row(
-                        modifier =  Modifier
-                            .padding(horizontal = 6.dp),
-                        horizontalArrangement =  Arrangement.Start,
-                    ) {
-                        Text(
-                            modifier =  Modifier
-                                .padding(horizontal = 6.dp),
-                            text = deviceInfo?.apiDevice?.name ?: "Loading..",
-                            color = Color.Black,
-                            fontWeight =  FontWeight.Medium,
-                            textAlign = TextAlign.Center,
-                        )
-
-                        Text(
-                            modifier =  Modifier
-                                .padding(end = 6.dp),
-                            text = "${deviceInfo?.currentVersion} >> ${deviceInfo?.latestVersion}" ?: "Loading..",
-                            color = Color.Gray,
-                            fontWeight =  FontWeight.Medium,
-                            fontStyle =  FontStyle.Italic,
-                            textAlign = TextAlign.Center,
-                        )
-                    }
-                    AnimatedVisibility(active) {
-                        Row(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(vertical = 8.dp, horizontal = 12.dp),
-                        ) {
-                            ElevatedCard  (
-                                Modifier,
-                                shape = RoundedCornerShape(8.dp),
-                            ){
-                                Column{
-                                    Text(
-                                        text = deviceInfo?.apiDevice?.name ?: "Loading...",
-                                        color = Color.Black,
-                                        fontWeight =  FontWeight.Bold,
-                                        textAlign = TextAlign.Center,
-                                    )
-                                    Text(
-                                        text = ("${deviceInfo?.apiDevice?.identifier}"),
-                                    )
-                                    Text(
-                                        text = ("Current version: ${deviceInfo?.currentVersion}"),
-                                    )
-                                    Text(
-                                        text = ("Latest version: ${deviceInfo?.latestVersion}"),
-                                    )
-                                }
-                            }
-                        }
-                    }
-                }
-                Row{
-
-                    Text(
-                        fileName,
-                        color = Color.Black,
-                    )
-                    Box{
-
-                        when (device.isUpToDate) {
-                            true -> Icon(Icons.Default.CheckCircle, "Up-to-date", tint = Color(0xFF4CAF50))
-                            false -> Icon(Icons.Default.Warning, "Needs update", tint = Color(0xFFF44336))
-                            null -> Icon(Icons.Default.ModeStandby, "Not checked", tint = Color(0xFFF44336))
-                        }
-                    }
-                }
-            }
-        }
-//        Column {
-//            AnimatedVisibility (!active) {
-//                Box(
-//
-//                ){
-//                    Text(
-//                        fileName,
-//                        color = Color.Black,
-//                    )
-//                }
-//            }
-//      }
-
-}
 
 @OptIn(DelicateCoroutinesApi::class)
 fun main() = application {
@@ -300,13 +177,20 @@ fun main() = application {
 
     var isLoading by remember { mutableStateOf(false) }
 
+    val fixedState = rememberWindowState(
+        position = WindowPosition.Aligned(Alignment.Center),
+        size     = DpSize(width = 800.dp, height = 600.dp)
+    )
+
 //    GlobalScope.launch {
 //        ApiCalls().fetchDevices()
 //    }
 
     Window(
         onCloseRequest = ::exitApplication,
-        title = "IPSW Manager"
+        title = "IPSW Manager",
+        state = fixedState,
+        resizable = false,
     ) {
         var folderPath by remember { mutableStateOf(lastPath) }
         var files      by remember { mutableStateOf(listOf<IpswFileStatus>()) }
@@ -327,11 +211,12 @@ fun main() = application {
         AppUI(
             folderPath = folderPath,
             files = files,
-            isLoading =  isLoading,
+            isLoading = isLoading,
             onSelectFolder = {
                 try {
                     UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
-                } catch (_: Exception) { /* ignore */ }
+                } catch (_: Exception) { /* ignore */
+                }
 
                 val chooser = JFileChooser().apply {
                     fileSelectionMode = JFileChooser.DIRECTORIES_ONLY
@@ -368,7 +253,7 @@ fun main() = application {
                         isLoading = false
                     }
                 }
-            }
+            },
         )
     }
 }
--- src/main/kotlin/operations/FileRow.kt
+++ src/main/kotlin/operations/FileRow.kt
@@ -1,2 +1,204 @@
 package operations
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.ModeStandby
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.PointerEventType
+import androidx.compose.ui.input.pointer.onPointerEvent
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import layout.bottomBorder
+
+@OptIn(ExperimentalComposeUiApi::class,
+    ExperimentalFoundationApi::class,
+    ExperimentalSharedTransitionApi::class
+)
+@Composable
+fun FileRow(
+    device: IpswFileStatus,
+    isLoading: Boolean,
+) {
+    val fileName = device.file.name
+    val operations = Operations()
+
+    var deviceInfo: Device? by remember { mutableStateOf<Device?>(null) }
+    var expandColumn by remember { mutableStateOf(false) }
+    var expandRow by remember { mutableStateOf(false) }
+    var detailsText by remember { mutableStateOf("See Details") }
+    var isLoading by remember { mutableStateOf(false) }
+
+    LaunchedEffect(device.file) {
+        deviceInfo = operations.buildDevice(device.file.name)
+    }
+
+    if (!expandColumn){
+        detailsText = "See Details"
+    } else {
+        detailsText = "Details"
+    }
+
+
+
+    Box(
+        modifier = Modifier
+            .padding(vertical = 2.dp,  horizontal = 12.dp)
+    ){
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp, horizontal = 12.dp)
+                .onPointerEvent(PointerEventType.Enter) {
+                    expandRow = true
+                }
+                .onPointerEvent(PointerEventType.Exit) {
+                    if (!expandColumn) expandRow = false
+                }
+                .combinedClickable(
+                    onClick = {
+                        expandColumn = !expandColumn
+                    },
+                )
+                .drawBehind {
+                    // convert 1.dp into pixels
+                    val strokeWidth = 1.dp.toPx()
+                    // left edge
+                    drawLine(
+                        color = Color.Black,
+                        start = Offset(0f, 0f),
+                        end   = Offset(0f, size.height),
+                        strokeWidth = strokeWidth
+                    )
+                }
+                .bottomBorder(0.5.dp, Color.Gray),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(8.dp, 4.dp)
+
+            ) {
+                Row(
+                    modifier =  Modifier
+                        .padding(end = 6.dp),
+                    horizontalArrangement =  Arrangement.Start,
+                    verticalAlignment = Alignment.Top
+                ) {
+                    AnimatedVisibility(expandRow){
+                        Text(detailsText)
+                    }
+                    Text(
+                        modifier =  Modifier
+                            .padding(horizontal = 6.dp),
+                        text = deviceInfo?.apiDevice?.name ?: "Loading..",
+                        color = Color.Black,
+                        fontWeight =  FontWeight.Medium,
+                        textAlign = TextAlign.Center,
+                    )
+
+                    Text(
+                        modifier =  Modifier
+                            .padding(end = 6.dp),
+                        text = "${deviceInfo?.currentVersion} >> ${deviceInfo?.latestVersion}" ?: "Loading..",
+                        color = Color.Gray,
+                        fontWeight =  FontWeight.Medium,
+                        fontStyle =  FontStyle.Italic,
+                        textAlign = TextAlign.Center,
+                    )
+                }
+                AnimatedVisibility(expandColumn) {
+                    expandedDetails(
+                        deviceInfo = deviceInfo,
+                    )
+                }
+            }
+            Row (
+                verticalAlignment = Alignment.Top
+            ){
+
+                Text(
+                    fileName,
+                    color = Color.Black,
+                )
+
+                Box{
+                    if(isLoading){
+                        CircularProgressIndicator()
+                    } else {
+                        when (device.isUpToDate) {
+                            true -> Icon(Icons.Default.CheckCircle, "Up-to-date", tint = Color(0xFF4CAF50))
+                            false -> Icon(Icons.Default.Warning, "Needs update", tint = Color(0xFFF44336))
+                            null -> Icon(Icons.Default.ModeStandby, "Not checked", tint = Color(0xFFF44336))
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+@OptIn(ExperimentalSharedTransitionApi::class)
+@Composable
+fun expandedDetails(
+    deviceInfo: Device?,
+){
+    Row(
+        modifier = Modifier
+            .padding(vertical = 8.dp, horizontal = 12.dp)
+
+    ) {
+        ElevatedCard  (
+            Modifier,
+            shape = RoundedCornerShape(8.dp),
+        ){
+            Column{
+                Text(
+                    text = deviceInfo?.apiDevice?.name ?: "Loading...",
+                    color = Color.Black,
+                    fontWeight =  FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                )
+                Text(
+                    text = ("${deviceInfo?.apiDevice?.identifier}"),
+                )
+                Text(
+                    text = ("Current version: ${deviceInfo?.currentVersion}"),
+                )
+                Text(
+                    text = ("Latest version: ${deviceInfo?.latestVersion}"),
+                )
+            }
+        }
+    }
+}
